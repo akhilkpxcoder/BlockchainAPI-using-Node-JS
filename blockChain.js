958448fc3f4ee883bfaca48cb2eb554f17bdd956
Akhil KP <98999402+akhilkpxcoder@users.noreply.github.com>
2022-06-27 02:09:50 +0530

    make it better

--- blockChain.js
+++ blockChain.js
@@ -1,24 +1,26 @@
 const Web3 = require("web3"); //used for connect with etherum network
 const sha = require("sha256"); //encrpyt the base64 value
 const fs = require("fs"); //read and write files
+const moment = require("moment");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const env = require("./env");
 var account = env.address; //address of etherum wallet
 var proofofexistence;
 const ContractAddress = require("./blockChainconfig.json").address;
-//getting json data of smart contract 
+//getting json data of smart contract
 let content = JSON.parse(
   fs.readFileSync("./build/contracts/ProofOfExistence.json", "utf8")
 );
-const provider = () => new HDWalletProvider({
-  privateKeys: env.privateKey,
-  providerOrUrl: `${env.host}`,
-  numberOfAddresses: 1
-});
+const provider = () =>
+  new HDWalletProvider({
+    privateKeys: env.privateKey,
+    providerOrUrl: `${env.host}`,
+    numberOfAddresses: 1,
+  });
 //connecting to web3
 async function web3Setup() {
-    //console.log(content,"content");
+  //console.log(content,"content");
   const web3 = createAlchemyWeb3(`${env.host}`, { writeProvider: provider() });
   proofofexistence = await new web3.eth.Contract(content.abi, ContractAddress);
   console.log("Web3 Connected");
@@ -26,7 +28,10 @@ async function web3Setup() {
 }
 //add document to blockchain
 const add = (req, res, next) => {
-  if (req.body.documentContent == null || req.body.documentContent == "undefined") {
+  if (
+    req.body.documentContent == null ||
+    req.body.documentContent == "undefined"
+  ) {
     res.status(400).send({
       error: "Could not get expected keyvalues in the JSON Request Payload",
     });
@@ -48,30 +53,31 @@ const add = (req, res, next) => {
             .send({ from: account })
             .then((result) => {
               const tnx = result;
-              if(tnx)
-              {
+              if (tnx) {
                 proofofexistence.methods
-                .returnData("0x" + inpHash)
-                .call({ from: account })
-                .then((result) => {
-                  if(result)
-                  {
-                    let date = new Date(result[0] * 1000);
-                    tnx["timestamp"] = date.toString();
-                    const response = {
+                  .returnData("0x" + inpHash)
+                  .call({ from: account })
+                  .then((result) => {
+                    if (result) {
+                      let date = moment
+                        .unix(result[0])
+                        .format('llll');
+                      tnx["timestamp"] = date.toString();
+                      const response = {
                         transaction: tnx,
                         documentHash: inpHash,
                       };
-                      res.status(200).send({ data: response, status: "Success" });
-                  }
-                });
+                      res
+                        .status(200)
+                        .send({ data: response, status: "Success" });
+                    }
+                  });
               }
-              
             })
             .catch((err) => {
-                res.status(400).send({
-                    status: "Failed",
-                  });
+              res.status(400).send({
+                status: "Failed",
+              });
             });
         }
       });
@@ -79,7 +85,10 @@ const add = (req, res, next) => {
 };
 //verify added document
 const verify = (req, res, next) => {
-  if (req.body.documentContent == null || req.body.documentContent == "undefined") {
+  if (
+    req.body.documentContent == null ||
+    req.body.documentContent == "undefined"
+  ) {
     res.status(400).send({
       error: "Could not get expected keyvalues in the JSON Request Payload",
     });
@@ -97,7 +106,7 @@ const verify = (req, res, next) => {
             .call({ from: account })
             .then((result) => {
               //console.log(result[0]);
-              const date = new Date(result[0] * 1000);
+              const date = moment.unix(result[0]).format('llll');
               res.status(200).send({
                 status: "Success",
                 timestamp: date.toString(),
