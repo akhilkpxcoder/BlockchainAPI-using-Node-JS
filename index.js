dc3fb944d0f83a9e7bc361b8bcc760caa02fc26b
Akhil KP <98999402+akhilkpxcoder@users.noreply.github.com>
2022-06-27 01:50:38 +0530

    make it better

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const blockChainRouter = require("./src/routes/blockChain");
-const { web3Setup } = require("./src/controllers/blockChain");
-const { add, verify } = require("./blockChain");
+const { add, verify, web3Setup } = require("./blockChain");
+
 //assign
 const app = express();
-web3Setup();
 const PORT = process.env.PORT || 5000;
 
 //use
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use("/", blockChainRouter);
+web3Setup();
 
 //route
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
